d259183148556b88ce5216f6f6066b64dfa0b38e
kiko-g <kikojpgoncalves@gmail.com>
2025-03-04 20:39:53 +0000

    og route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,57 +25,31 @@ export const metadata: Metadata = {
     "Consumer Price Index",
     "Real Purchasing Power",
   ],
-  authors: [{ name: siteConfig.author, url: siteConfig.links.website }],
+  authors: [
+    {
+      name: siteConfig.author,
+      url: siteConfig.links.website,
+    },
+  ],
   creator: siteConfig.author,
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#000000" }],
   openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
     images: [
       {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 680,
-        alt: siteConfig.name,
+        url: `/og?title=${encodeURIComponent(
+          siteConfig.name,
+        )}&description=${encodeURIComponent(siteConfig.description)}`,
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: siteConfig.socialhandle,
-  },
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    images: [
+      {
+        url: `/og?title=${encodeURIComponent(
+          siteConfig.name,
+        )}&description=${encodeURIComponent(siteConfig.description)}`,
+      },
     ],
-    apple: "/apple-touch-icon.png",
-    shortcut: "/favicon-16x16.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: false,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
   },
 }
 
