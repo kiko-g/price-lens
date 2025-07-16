60e05b9674d43ba675842f526e540354486e428d
kiko-g <kikojpgoncalves@gmail.com>
2025-07-16 10:13:12 +0100

    .

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -48,16 +48,25 @@ export async function signup(formData: FormData) {
 
 export async function signInWithGoogle() {
   const supabase = await createClient()
-  const origin = (await headers()).get("origin")
 
-  if (!origin) {
-    return redirect("/login?error=origin-missing")
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel
+      "http://localhost:3000/"
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+    return url
   }
+
+  const redirectURL = getURL()
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${origin}/auth/callback`,
+      redirectTo: `${redirectURL}auth/callback`,
     },
   })
 
