f916a41b457d0c4b66fdd7514a8ef61190fb48bb
kiko-g <kikojpgoncalves@gmail.com>
2025-09-15 15:52:18 +0100

    .

--- src/components/layout/SearchDialogQuickNav.tsx
+++ src/components/layout/SearchDialogQuickNav.tsx
@@ -12,7 +12,7 @@ export function SearchDialogQuickNav() {
   const [isExpanded, setIsExpanded] = useState(false)
 
   return (
-    <div className="fixed right-4 bottom-4 z-50 flex flex-col items-end justify-center gap-3 md:right-8 md:bottom-8">
+    <div className="fixed right-5 bottom-7 z-50 flex flex-col items-end justify-center gap-3 md:right-8 md:bottom-10">
       <SearchDialog>
         <Button
           variant="default"
